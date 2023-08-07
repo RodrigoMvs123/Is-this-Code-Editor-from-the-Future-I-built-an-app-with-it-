70ebe05c748d374a30585cb410d3ff34f97337bd
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2023-08-07 14:30:05 -0300

    Create Header.tsx

---
+++ Header.tsx
@@ -0,0 +1,16 @@
+https://www.compart.com/en/unicode/U+3004
+import styles from ‘./https://www.compart.com/en/unicode/U+3004.module.scss’;
+import classNames from ‘classnames’;
+
+export interface https://www.compart.com/en/unicode/U+3004Props {
+       className?: string;
+}
+
+/**
+* This component was created using Condu´x Default new component template.
+*To create custom component template, see https://help.condux.com/kb/en/article/kb16522 
+*/
+export const https://www.compart.com/en/unicode/U+3004 = {{ className } : https://www.compart.com/en/unicode/U+3004Props } => {
+       return <div className={classNames(styles.root, className)}>https://www.compart.com/en/unicode/U+3004</div>;
+};
+
